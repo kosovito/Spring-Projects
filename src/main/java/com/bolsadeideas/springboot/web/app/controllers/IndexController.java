c20ea05af080a7fb42421207ad913648e9d4b978
kosovito <kosovito@gmail.com>
2022-12-13 22:41:48 +0100

    131222

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,13 +1,29 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-		@GetMapping({"/index", "/", "/home"})
-		public String index() {
+		@GetMapping({"/index", "/", "", "/home"})
+		public String index(Model model) {
+			model.addAttribute("titulo", "hola Spring Framework con Model!");
 			return "index";
+		}
+		
+		@RequestMapping("/perfil")
+		public String perfil(Model model) {
+			Usuario usuario = new Usuario();
+			usuario.setNombre("Álvaro");
+			usuario.setApellido("García");
+			model.addAttribute("usuario", usuario);
+			model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+			return "perfil";
 		}
 }
